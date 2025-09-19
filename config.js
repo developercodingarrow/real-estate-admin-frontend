0efdb36463e630547c4fe411cc51f5b0b962005b
developercodingarrow <digitalcloudinfo@gmail.com>
2025-09-20 02:02:24 +0530

    change config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 // config.js
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_PRODUCTION === "false"
-    ? process.env.NEXT_PUBLIC_API_DEVELOPMENT
-    : process.env.NEXT_PUBLIC_API_PRODUCTION;
+export const API_BASE_URL = NEXT_PUBLIC_API_PRODUCTION;
